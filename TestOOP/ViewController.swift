c9de89d4bd52a1173d2f4ec04abd02df3ffa9323
Rizky Adipratama <adipratama.rizky@gmail.com>
2019-05-15 09:23:53 +0700

    Initial Commit

---
+++ TestOOP/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestOOP
+//
+//  Created by Rizky Adipratama Ruddyar on 15/05/19.
+//  Copyright © 2019 Rizky Adipratama Ruddyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
